af06258c1aab132d018ef7c38f31103ff4e4c2e3
Asus <newg91945@gmail.com>
2026-08-21 00:50:42 +0530

    Added more code to python file

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from datetime import datetime
 
 today = datetime.today()
 print(today)
+print("Date : ",today.day)
+print("Month : ", today.month)
+print("Year : ", today.year)
+print("Hour : ", today.hour)
+print("Minute : ", today.minute)
+print("Second : ", today.second)
+print("Micro seconds : ", today.microsecond)
 print("This isnide the Github actions")
